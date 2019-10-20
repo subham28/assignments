79fe0334088bad91236d3282e6ae7be1a98738ea
Subham Biswas <subham.biswas981@gmail.com>
2019-10-20 13:29:27 +0530

    committing search tweets changes

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -1,7 +1,20 @@
 import Header from "./Headers";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { Spin, Table } from "antd";
 
+const coloumn = [
+  {
+    title: "Row Number",
+    dataIndex: "key",
+    key: "key"
+  },
+  {
+    title: "Tweets",
+    dataIndex: "tweet",
+    key: "tweet"
+  }
+];
 export default class HomePage extends Component {
   static propTypes = {
     user: PropTypes.shape({
@@ -17,7 +30,9 @@ export default class HomePage extends Component {
     user: {},
     error: null,
     authenticated: false,
-    searchText : null
+    searchText: null,
+    searchedTweets: [],
+    loader: false
   };
 
   componentDidMount() {
@@ -55,24 +70,51 @@ export default class HomePage extends Component {
 
   setSearchCriteria = ({ target }) => {
     this.setState({
-      searchText:target.value
-    })
-  }
-  callSearchApi = () => {
-    fetch("http://localhost:4000/auth/search/tweet",{
+      searchText: target.value
+    });
+  };
+  callSearchApi = async () => {
+    this.setState({
+      loader: true
+    });
+    await fetch("http://localhost:4000/auth/search/tweet", {
       method: "POST",
       credentials: "include",
-      body:JSON.stringify({"text":this.state.searchText}),
+      body: JSON.stringify({ text: this.state.searchText }),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
         "Access-Control-Allow-Credentials": true
       }
-    }).then(res => {
-      console.log(res);
-      
+    }).then(result => {
+      //console.log(result.json());
+      result.json().then(data => {
+        var resArr = [];
+        data.responseObj.map((value, index) => {
+          resArr.push({ key: index, tweet: value });
+        });
+        this.setState({
+          searchedTweets: resArr,
+          loader: false
+        });
+      });
+    });
+    //var response = callApifun();
+  };
+  /* callApifun = () => {
+    return new Promise(function(resolve,reject){
+      fetch("http://localhost:4000/auth/search/tweet", {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({ text: this.state.searchText }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+      }
+    }).then()
     })
-  }
+  } */
   render() {
     const { authenticated } = this.state;
     console.log(this.state);
@@ -81,25 +123,41 @@ export default class HomePage extends Component {
     //   this.props.history.push("/search");
     // }
     return (
-      <div>
-        <Header
-          authenticated={authenticated}
-          handleNotAuthenticated={this._handleNotAuthenticated}
-        />
+      <Spin spinning={this.state.loader}>
         <div>
-          {!authenticated ? (
-            <h1>Welcome!</h1>
-          ) : (
-            <div>
-              <h1>You have login succcessfully!</h1>
-              <h2>Welcome {this.state.user.name}!</h2>
-              <label>Search tweets: </label>
-              <input type="text" placeholder="Enter hashtag" onChange={this.setSearchCriteria}></input>
-              <button onClick={this.callSearchApi}>Search</button>
-            </div>
-          )}
+          <Header
+            authenticated={authenticated}
+            handleNotAuthenticated={this._handleNotAuthenticated}
+          />
+          <div>
+            {!authenticated ? (
+              <h1>Welcome!</h1>
+            ) : (
+              <div>
+                <h1>You have login succcessfully!</h1>
+                <h2>Welcome {this.state.user.name}!</h2>
+                <label>Search tweets: </label>
+                <input
+                  type="text"
+                  placeholder="Enter hashtag"
+                  onChange={this.setSearchCriteria}
+                ></input>
+                <button onClick={this.callSearchApi}>Search</button>
+                <div>
+                  {this.state.searchedTweets.length > 0 ? (
+                    <Table
+                      dataSource={this.state.searchedTweets}
+                      columns={coloumn}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </Spin>
     );
   }
 }
